9622ffa174de9b2e4b082eb3cc58877c66bbfdac
Ioan Loosley <legit.ioangogo@gmail.com>
2016-09-19 14:04:04 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -22,15 +22,13 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 var oldjson;
+var incbible = true;
 
 function genboard() {
     $.ajax({
         type: "POST",
         url: "/api/service/list",
         success: function(response) {
-            console.log(response);
-
-
             var json_obj = response; //parse JSON
             if (JSON.stringify(json_obj) != oldjson) {
                 var songoutput = "<div id='songs'><h1 id='title'>Songs</h1><br><div class='items'>";
@@ -39,11 +37,10 @@ function genboard() {
                     if (json_obj.results.items[i].plugin == "songs") {
                         var classes = json_obj.results.items[i].selected ? "sellected" : "";
                         songoutput += "<span class='item top " + classes + "'>" + json_obj.results.items[i].title + "</span><br>";
-                    } else if (json_obj.results.items[i].plugin == "bibles") {
+                    } else if (json_obj.results.items[i].plugin == "bibles" && incbible == true) {
                         var classes = json_obj.results.items[i].selected ? "sellected" : "";
                         bibleoutput += "<span class='item " + classes + "'>" + json_obj.results.items[i].title.split(", ")[0] + "</span><br>";
                     }
-
                 }
                 songoutput += "</div></div>";
                 bibleoutput += "</div></div>";
